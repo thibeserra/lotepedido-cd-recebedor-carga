8d07eeaba5f74a34a63ec52345d776c140ca9b4c
thiago beserra <thiago.beserra@luizalabs.com>
2019-02-12 16:56:57 -0200

    iniciou estrutura do projeto

---
+++ src/main/java/br/com/sapri/lotepedidocdrecebedorcarga/lotepedidocdrecebedorcarga/LotepedidoCdRecebedorCargaApplication.java
@@ -0,0 +1,14 @@
+package br.com.sapri.lotepedidocdrecebedorcarga.lotepedidocdrecebedorcarga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotepedidoCdRecebedorCargaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotepedidoCdRecebedorCargaApplication.class, args);
+	}
+
+}
+
